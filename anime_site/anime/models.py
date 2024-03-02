45c370e954b624e468c20882d3dc8e52144103c1
Destrifer <destrifer@yandex.ru>
2024-03-02 22:00:42 +0300

    Next time

--- anime_site/anime/models.py
+++ anime_site/anime/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	login = models.CharField(max_length=100)
+	password = models.CharField(max_length=100)
+	email = models.EmailField()
+	date = models.DateField(auto_now_add=True)
+
+
+class Anime(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	date = models.DateField()
+	rating = models.SmallIntegerField(default=0)
+	poster = models.ImageField(blank=True)
+
+class Episode(models.Model):
+	anime = models.SmallIntegerField()
+	number = models.SmallIntegerField()
+	title = models.CharField(max_length=100)
+	time = models.TimeField()
+	video = models.FileField()
+
+class Comments(models.Model):
+	user = models.SmallIntegerField()
+	anime = models.SmallIntegerField()
+	text = models.TextField()
+	date = models.DateField(auto_now_add=True)
+	rating = models.SmallIntegerField(default=0)
