ade941609863e69a94bd959e83103d21ae55a49a
Destrifer <destrifer@yandex.ru>
2024-03-03 21:19:12 +0300

    Добавил админку

--- anime_site/anime/admin.py
+++ anime_site/anime/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Anime, Comments, User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('login', 'password', 'email')
+
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'anime', 'comment_text')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Anime, AnimeAdmin)
+admin.site.register(Comments, CommentsAdmin)
